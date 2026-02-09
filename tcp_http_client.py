803babeac882cc923d65c193e6c4664ec66b3cc4
EliasKarj <elias1.karjalainen@gmail.com>
2026-02-09 13:53:06 +0200

    Add TCP HTTP client implementation for basic GET request

---
+++ tcp_http_client.py
@@ -0,0 +1,14 @@
+import socket as sc
+
+s = sc.socket(sc.AF_INET, sc.SOCK_STREAM)
+try:
+    s.connect(("www.cc.puv.fi", 80))
+    print("Connected to www.cc.puv.fi:80")
+except sc.error:
+    print("Connection failed")
+    exit()
+
+s.send(b"GET /~gc/index.html HTTP/1.1\r\nHost: www.cc.puv.fi\r\n\r\n")
+msg = s.recv(2048)
+print(msg.decode("ascii"))
+s.close()
